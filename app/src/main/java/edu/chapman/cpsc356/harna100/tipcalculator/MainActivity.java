e54bbe119bb0003b181f6bdbd4dc0c75e478cd32
Paul Harnack <harna100@mail.chapman.edu>
2017-09-07 18:49:06 -0700

    Lays out basic skeleton in MainActivity

--- app/src/main/java/edu/chapman/cpsc356/harna100/tipcalculator/MainActivity.java
+++ app/src/main/java/edu/chapman/cpsc356/harna100/tipcalculator/MainActivity.java
@@ -2,12 +2,61 @@ package edu.chapman.cpsc356.harna100.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+	private EditText et_mealPrice;
+	private Button btn_clear;
+	private RatingBar rb_ServiceRating;
+	private TextView tv_tipPrice;
+	private TextView tv_totalPrice;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+
 	}
+
+
+	private void calcTip(){
+
+	}
+
+	private double getTipPercentage(){
+		return 0;
+	}
+
+	private int convertRatingBarScore(){
+		return 0;
+	}
+
+	private void setTipPrice(double toSet){
+
+	}
+
+	private void setMealPrice(double toSet){
+
+	}
+
+	private String convertStringToCurrency(String toConvert){
+		return null;
+	}
+
+	private void clear(){
+		// clear edit text
+
+		// reset rating bar
+
+		// reset tip price
+
+		// reset total price
+	}
+
 }
